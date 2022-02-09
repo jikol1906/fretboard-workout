02998f9318eef6e09c7ca6aeedf099fd7f75ccaf
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-09 21:32:29 +0100

    wip

--- src/Utils/Types.ts
+++ src/Utils/Types.ts
@@ -2,3 +2,7 @@ export type Note = string;
 export type FretboardPosition = Note
 export type Fretboard = FretboardPosition[][];
 export type PointerPosition = [string:number,fret:number];
+
+
+type NaturalNotes = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G'
+type NoteType = `${NaturalNotes}#/${NaturalNotes}b`
