f6cbad28dec94924e51ed1d8b6c22aea355f0505
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-10 11:00:22 +0100

    added disabled to buttons

--- src/Components/GuessNote/NoteButtons/NoteButtons.tsx
+++ src/Components/GuessNote/NoteButtons/NoteButtons.tsx
@@ -71,10 +71,10 @@ export const NoteButtons: React.FC<INoteButtonsProps> = ({ disabled }) => {
 
   return (
     <Grid sx={noteButtonGridStyles} gap="0">
-      <Button ref={btn1ref} onClick={() => btnClicked(buttons[0])} style={{gridArea: "b1"}} variant="noteBtn">{b1}</Button>
-      <Button ref={btn2ref} onClick={() => btnClicked(buttons[1])} style={{gridArea: "b2"}} variant="noteBtn">{b2}</Button>
-      <Button ref={btn3ref} onClick={() => btnClicked(buttons[2])} style={{gridArea: "b3"}} variant="noteBtn">{b3}</Button>
-      <Button ref={btn4ref} onClick={() => btnClicked(buttons[3])} style={{gridArea: "b4"}} variant="noteBtn">{b4}</Button>
+      <Button disabled={disabled} ref={btn1ref} onClick={() => btnClicked(buttons[0])} style={{gridArea: "b1"}} variant="noteBtn">{b1}</Button>
+      <Button disabled={disabled} ref={btn2ref} onClick={() => btnClicked(buttons[1])} style={{gridArea: "b2"}} variant="noteBtn">{b2}</Button>
+      <Button disabled={disabled} ref={btn3ref} onClick={() => btnClicked(buttons[2])} style={{gridArea: "b3"}} variant="noteBtn">{b3}</Button>
+      <Button disabled={disabled} ref={btn4ref} onClick={() => btnClicked(buttons[3])} style={{gridArea: "b4"}} variant="noteBtn">{b4}</Button>
     </Grid>
   );
 };
