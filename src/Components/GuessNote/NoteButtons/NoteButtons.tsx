a812e5b48b0e9426bb55a0034a5184f27efe5be6
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-11 18:45:46 +0100

    update wrongClickedButtons on wrong button click

--- src/Components/GuessNote/NoteButtons/NoteButtons.tsx
+++ src/Components/GuessNote/NoteButtons/NoteButtons.tsx
@@ -19,6 +19,7 @@ export const NoteButtons: React.FC<INoteButtonsProps> = ({ disabled }) => {
   const dispatch = useAppDispatch();
   const buttons = useAppSelector(selectNoteButtons);
   const correctAnswer = useAppSelector(selectCorrectAnswer);
+  const [wrongClickedButtons,setWrongClickedButtons] = useState([false,false,false,false])
 
   const btn1ref = useRef<HTMLButtonElement>(null);
   const btn2ref = useRef<HTMLButtonElement>(null);
@@ -33,14 +34,18 @@ export const NoteButtons: React.FC<INoteButtonsProps> = ({ disabled }) => {
     return <span style={{height:'1.5em'}}>{b}</span>;
   });
 
-  const btnClicked = (val: string) => {
+  const btnClicked = (val: string,btnNumber:number) => {
     if (val === correctAnswer) {
       dispatch(setWrongAnswerClicked(false));
       dispatch(incrementCorrectAnswered());
       dispatch(incrementTotalAnswered());
     } else {
-      dispatch(setWrongAnswerClicked(true));
-      dispatch(incrementTotalAnswered());
+      setWrongClickedButtons(prev => {
+        prev[btnNumber] = true
+        return prev
+      })
+      // dispatch(setWrongAnswerClicked(true));
+      // dispatch(incrementTotalAnswered());
     }
   };
 
@@ -69,10 +74,10 @@ export const NoteButtons: React.FC<INoteButtonsProps> = ({ disabled }) => {
 
   return (
     <Grid sx={noteButtonGridStyles} gap="0">
-      <Button disabled={disabled} ref={btn1ref} onClick={() => btnClicked(buttons[0])} style={{gridArea: "b1"}} variant="noteBtn">{b1}</Button>
-      <Button disabled={disabled} ref={btn2ref} onClick={() => btnClicked(buttons[1])} style={{gridArea: "b2"}} variant="noteBtn">{b2}</Button>
-      <Button disabled={disabled} ref={btn3ref} onClick={() => btnClicked(buttons[2])} style={{gridArea: "b3"}} variant="noteBtn">{b3}</Button>
-      <Button disabled={disabled} ref={btn4ref} onClick={() => btnClicked(buttons[3])} style={{gridArea: "b4"}} variant="noteBtn">{b4}</Button>
+      <Button disabled={disabled} ref={btn1ref} onClick={() => btnClicked(buttons[0],0)} style={{gridArea: "b1"}} variant="noteBtn">{b1}</Button>
+      <Button disabled={disabled} ref={btn2ref} onClick={() => btnClicked(buttons[1],1)} style={{gridArea: "b2"}} variant="noteBtn">{b2}</Button>
+      <Button disabled={disabled} ref={btn3ref} onClick={() => btnClicked(buttons[2],2)} style={{gridArea: "b3"}} variant="noteBtn">{b3}</Button>
+      <Button disabled={disabled} ref={btn4ref} onClick={() => btnClicked(buttons[3],3)} style={{gridArea: "b4"}} variant="noteBtn">{b4}</Button>
     </Grid>
   );
 };
