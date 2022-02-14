9960b3814f7add9965cff9ef1895373cb78be67f
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-14 11:01:31 +0100

    fixed modal overflow

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -15,15 +15,15 @@ const Modal: React.FunctionComponent<IModalProps> = ({ show, children }) => {
       style={{
         overlay: {
           backgroundColor: "rgba(0,0,0,.7)",
+          display: 'flex',
+
         },
         content: {
-          top: "50%",
-          left: "50%",
-          right: "auto",
-          bottom: "auto",
-          marginRight: "-50%",
-          transform: "translate(-50%, -50%)",
+          position: 'static',
+          margin:'auto',
           padding: "0",
+          maxHeight:'100vh',
+          overflowY:'auto'
         },
       }}
     >
