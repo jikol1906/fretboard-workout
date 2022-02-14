dd6791015ed429df1e6f52403df2811e61559627
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-14 14:32:54 +0100

    show total and correct and add click handler to go to main menu button

--- src/Components/GuessNote/Gamestats/Gamestats.tsx
+++ src/Components/GuessNote/Gamestats/Gamestats.tsx
@@ -1,13 +1,19 @@
 /** @jsxImportSource theme-ui */
 
+import { useDispatch } from "react-redux";
 import { Box, Button, Grid, Heading, Text } from "theme-ui";
+import { selectTotalandCorrectAnswered, setGameStarted } from "../../../redux/guessNoteSlice";
+import { useAppSelector } from "../../../redux/hooks";
 import Modal from "../../Modal/Modal";
 
 interface IGamestatsProps {
     onTryAgainClicked:() => void
 }
 
-const Gamestats: React.FunctionComponent<IGamestatsProps> = ({onTryAgainClicked}) => (
+const Gamestats: React.FunctionComponent<IGamestatsProps> = ({onTryAgainClicked}) => {
+  const dispatch = useDispatch();
+  const [total, correct] = useAppSelector(selectTotalandCorrectAnswered);
+  return (
   <Modal show={true}>
     <Grid
       gap="3"
@@ -24,19 +30,20 @@ const Gamestats: React.FunctionComponent<IGamestatsProps> = ({onTryAgainClicked}
       <Grid gap="1" sx={{ alignContent: "center" }}>
         <Text as="p" sx={{ fontSize: "1.5rem", display: "flex" }}>
           <span style={{ flex: "1" }}>total:</span>
-          <span>6</span>
+          <span>{total}</span>
         </Text>
         <Text as="p" sx={{ fontSize: "1.5rem", display: "flex" }}>
           <span style={{ flex: "1" }}>correct:</span>
-          <span>6</span>
+          <span>{correct}</span>
         </Text>
       </Grid>
       <Grid gap="2">
-        <Button>Main menu</Button>
+        <Button onClick={(e) => dispatch(setGameStarted(false))}>Main menu</Button>
         <Button onClick={onTryAgainClicked}>try again</Button>
       </Grid>
     </Grid>
   </Modal>
-);
+  )
+};
 
 export default Gamestats;
