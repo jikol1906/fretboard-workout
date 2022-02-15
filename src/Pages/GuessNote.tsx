5c27a1031e95b42a1245347e3b1260666f7ed1ab
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-15 17:27:16 +0100

    GuessNote : less padding on sides

--- src/Pages/GuessNote.tsx
+++ src/Pages/GuessNote.tsx
@@ -18,7 +18,7 @@ const GuessNote: React.FunctionComponent = () => {
       sx={{
         height: "100%",
         gridTemplateRows: "1fr 1fr",
-        padding: "2",
+        padding: (gameStarted ? "2rem 1rem":"4rem 0"),
         gap: "2",
         [maxHeightbpSmall]: {
           gridTemplateRows: "auto 1fr",
